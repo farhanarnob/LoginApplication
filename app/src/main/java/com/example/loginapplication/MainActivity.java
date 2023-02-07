92a888a16b99e43fd5ee103edb554184085eb8b4
Farhan Rahman Arnob <this.farhanarnob@gmail.com>
2023-02-06 22:42:20 -0600

    Gym Login

---
+++ app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.loginapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogOut;
+    FirebaseAuth mAuth;
+    TextInputEditText loginEmail;
+    ProgressDialog loading;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogOut = findViewById(R.id.btnLogout);
+        loginEmail = findViewById(R.id.loginEmail);
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            String email = "";
+            for (UserInfo profile : user.getProviderData()) {
+                email = profile.getEmail();
+            }
+            loading = ProgressDialog.show(this, "Loading", "Please Wait", false, true);
+            loginEmail.setText(email);
+            updateCountForUser(email);
+        }
+
+        btnLogOut.setOnClickListener(view ->{
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    }
+
+    private void updateCountForUser (String email){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, BuildConfig.APPLICATION_ID + "action=update&Username=" + email,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("Success", "onResponse: Count Updated");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+        );
+        int socketTimeOut = 50000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
+        loading.dismiss();
+    }
+
+}
